6195e52ef6419aaa3e59c8e94510692ba31356c4
Niyati Desai <niyatihd@gmail.com>
2019-04-19 21:22:38 -0700

    Solve 12.js reverse string

---
+++ CTCI/Chap1_arrays_and_strings/12.js
@@ -0,0 +1,30 @@
+// Reverse string
+// ex1: str = "strong" => "gnorts"
+// ex1: str = "later" => "retal"
+
+// *-------------*
+// Reverse string
+// *-------------*
+
+function reversestr(str) {
+  const mid = Math.floor(str.length / 2);
+  const last = str.length - 1;
+  let tempArr = str.split("");
+
+  for (let i = 0; i < mid; i++) {
+    let temp = tempArr[i];
+    tempArr[i] = tempArr[last - i];
+    tempArr[last - i] = temp;
+  }
+
+  return tempArr.join("");
+}
+
+// Analysis: 
+// Time complexity = O(n)
+// Space complexity = O(n)
+
+
+// TEST CASES
+console.log(reversestr("strong"));
+console.log(reversestr("abcba"));
