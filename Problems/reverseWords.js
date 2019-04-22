7962b515382474aa23b306b134b22c67fd8cd494
Niyati Desai <niyatihd@gmail.com>
2019-04-21 18:43:58 -0700

    Function reverseWord solved

--- Problems/reverseWords.js
+++ Problems/reverseWords.js
@@ -2,12 +2,27 @@
 // ex: let stringToReverse = "cake pound Steal" 
 // reverseWords(stringToReverse) => "Steal pound cake"
 
-function (sentence) {
-  let tempArr = str.split(" ");
+function reverseWords(sentence) {
+  let tempArr = sentence.split(" ");
   let lastIdx = tempArr.length - 1;
 
-  for (let i = 0; i < tempArr; i++) {
+  for (let i = 0; i < tempArr.length / 2; i++) {
     let temp = tempArr[i];
-    tempArr[i] = tempArr[lastIdx]
+    tempArr[i] = tempArr[lastIdx - i];
+    tempArr[lastIdx - i] = temp;
   }
+
+  return tempArr.join(" ");
 }
+
+// *-------------*
+// Analysis: 
+// *-------------*
+// Time complexity = O(log(n))
+// Space complexity = O(n)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
+let stringToReverse = "cake pound Steal";
+console.log(reverseWords(stringToReverse));
