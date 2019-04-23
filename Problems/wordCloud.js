52b59ae8a178d04058f7c6a167a289867880a999
Niyati Desai <niyatihd@gmail.com>
2019-04-23 16:34:34 -0700

    Solve wordCloud

--- Problems/wordCloud.js
+++ Problems/wordCloud.js
@@ -28,25 +28,47 @@ function wordCloud(str) {
   return count;
 }
 
-function wordCloud2(str) {
-  // let count = {};
+// *-------------*
+// Option#2
+// *-------------*
+
+function splitWords(str) {
   let start = 0;
   let wordLength = 0;
   const words = [];
 
   for (let i = 0; i < str.length; i++) {
-    if (isLetter(str[i])) {
+    const char = str[i];
+
+    if (isLetter(char)) {
       if (wordLength === 0) {
         start = i;
       }
       wordLength += 1;
-    } else if (!isLetter(str[i])) {
-      words.push(str.slice(start, start + wordLength));
+    } else if (!isLetter(char)) {
+      let tempWord = str.slice(start, start + wordLength);
+      if (tempWord.length > 0) words.push(str.slice(start, start + wordLength));
       wordLength = 0;
     }
   }
 
   return words;
+}
+
+function wordCloud2(str) {
+  const words = splitWords(str);
+
+  let count = {};
+
+  for (let i = 0; i < words.length; i++) {
+    if (count[words[i]]) {
+      count[words[i]] += 1;
+    } else {
+      count[words[i]] = 1;
+    }
+  }
+
+  return count;
 }
 
 // *-------------*
@@ -58,7 +80,12 @@ function wordCloud2(str) {
 // *-------------*
 //   TEST CASES
 // *-------------*
-console.log(wordCloud("After beating the eggsd Dana read the next step: "));
-console.log(wordCloud("Add milk and eggs, then add flour and sugar."));
-console.log(wordCloud2("After beating the eggsd Dana read the next step: "));
+// console.log(wordCloud("After beating the eggsd Dana read the next step: "));
+// console.log(wordCloud("After beating the eggs, Dana read the next step: "));
+// console.log(wordCloud("Add milk and eggs, then add flour and sugar."));
+// console.log(splitWords("After beating the eggsd Dana read the next step: "));
+// console.log(splitWords("Add milk and eggs, then add flour and sugar."));
+// console.log(splitWords("I love Nishika. "));
+console.log(wordCloud2("After beating the eggs Dana read the next step: egg "));
 console.log(wordCloud2("Add milk and eggs, then add flour and sugar."));
+console.log(wordCloud2("I love Nishika. "));
