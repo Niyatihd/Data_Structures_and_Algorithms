3e5aa2a75152cdc304c6a5655b3a7c947b79ff7d
Niyati Desai <niyatihd@gmail.com>
2019-04-23 20:15:13 -0700

    Solve topScores

--- Problems/topScores.js
+++ Problems/topScores.js
@@ -6,3 +6,15 @@
 // const unsortedScores = [37, 89, 41, 65, 91, 53];
 // const HIGHEST_POSSIBLE_SCORE = 100;
 // sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE); => [91, 89, 65, 53, 41, 37]
+
+function topScores(unsortedScores, highestPossibleScore) {
+  const scores = new Array(highestPossibleScore + 1).fill(0); //starts with 0, so length is 1 more than highestPossibleScore
+
+  for (let i = 0; i < unsortedScores.length; i++) {
+    scores[unsortedScores[i]] += 1;
+  }
+
+
+
+
+}
