4b590e5fc0a10f489e1b291a203f082812effd98
Niyati Desai <niyatihd@gmail.com>
2019-04-23 20:21:20 -0700

    Solve topScores

--- Problems/topScores.js
+++ Problems/topScores.js
@@ -9,12 +9,34 @@
 
 function topScores(unsortedScores, highestPossibleScore) {
   const scores = new Array(highestPossibleScore + 1).fill(0); //starts with 0, so length is 1 more than highestPossibleScore
+  let sorted = [];
 
   for (let i = 0; i < unsortedScores.length; i++) {
     scores[unsortedScores[i]] += 1;
   }
+  console.log(scores);
 
+  for (let score = scores.length - 1; score >= 0; score--) {
 
+    // if (scores[score] > 0) {
+    for (let k = 0; k < scores[score]; k++) {
+      sorted.push(score);
+    }
+    // }
+  }
 
-
+  return sorted;
 }
+
+// *-------------*
+// Analysis: 
+// *-------------*
+// Time complexity = O(n)
+// Space complexity = O(n)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
+const unsortedScores = [37, 89, 41, 65, 91, 53];
+const HIGHEST_POSSIBLE_SCORE = 100;
+console.log(topScores(unsortedScores, HIGHEST_POSSIBLE_SCORE));
