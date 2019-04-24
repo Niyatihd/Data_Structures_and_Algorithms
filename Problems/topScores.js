a5e3d4020d48c03b00b2cb81d4224ee61f965572
Niyati Desai <niyatihd@gmail.com>
2019-04-23 20:03:03 -0700

    Solve topScores

---
+++ Problems/topScores.js
@@ -0,0 +1,8 @@
+// Write a function that takes:
+// an array of unsortedScores
+// the highestPossibleScore in the game
+// and returns a sorted array of scores in less than O(n\lg{n})O(nlgn) time.
+// For example:
+// const unsortedScores = [37, 89, 41, 65, 91, 53];
+// const HIGHEST_POSSIBLE_SCORE = 100;
+// sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE); => [91, 89, 65, 53, 41, 37]
