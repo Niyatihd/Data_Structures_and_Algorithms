65feab7e42e7d4f0920d6054ca9d9ca74374e8b1
Niyati Desai <niyatihd@gmail.com>
2019-04-25 21:25:52 -0700

    Solve highest product of the input array

--- Problems/highestProduct.js
+++ Problems/highestProduct.js
@@ -9,17 +9,57 @@
 // min(set) <= next ? 
 // if yes,  delete min, add next => 
 
+function createHighestNumObj(arr) {
+  let num1 = Math.max(arr[0], arr[1], arr[2]);
+  let num3 = Math.min(arr[0], arr[1], arr[2]);
+  let obj = {
+    1: num1,
+    3: num3
+  };
+
+  for (let i = 0; i < 3; i++) {
+    if (arr[i] !== num1 && arr[i] !== num3) {
+      obj[2] = arr[i];
+    }
+  }
+
+  return obj;
+}
+
+// console.log(createHighestNumObj([1, 2, 3, 5, 6]));
+
 function highestProdofThreeInts(arr) {
-  let highestNums = new Set([arr[0], arr[1], arr[2]]);
+
+
+  let highestNums = createHighestNumObj(arr);
+  // let highestNums = arr.slice(0, 3);
+  // let highestNums = new Set([arr[0], arr[1], arr[2]]);
+  console.log("highestNums: ");
+  console.log(highestNums);
 
   for (let i = 3; i < arr.length; i++) {
+    if (highestNums[3] < arr[i]) {
+      highestNums[3] = arr[i];
+    } else if (highestNums[2] < arr[i]) {
+      highestNums[2] = arr[i];
+    } else if (highestNums[1] < arr[i]) {
+      highestNums[1] = arr[i];
+    }
 
+    highestNums = createHighestNumObj(Object.values(highestNums));
+    console.log("highestNums: ");
+    console.log(highestNums);
   }
-  return highestNums;
+
+  console.log("Final highestNums: ");
+  console.log(highestNums);
+
+  let result = highestNums[1] * highestNums[2] * highestNums[3];
+  return result;
 }
 
 // *-------------*
 //   TEST CASES
 // *-------------*
-const test = [1, 2, 3, 5, 6];
+const test = [1, 9, 3, 5, 6, 8];
 console.log(highestProdofThreeInts(test));
