94b1e95bd1724fdd586cd2ac15e2a1c8551268ad
Niyati Desai <niyatihd@gmail.com>
2019-05-07 12:14:47 -0700

    Solve twoSum

--- LC_Problems/two_sum.js
+++ LC_Problems/two_sum.js
@@ -4,3 +4,30 @@
 // Given nums = [2, 7, 11, 15], target = 9,
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
+
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let ele1 = arr[i];
+
+    for (let j = i + 1; j < arr.length; j++) {
+      let ele2 = arr[j];
+
+      if (ele1 + ele2 === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+// *-------------*
+// Analysis:
+// *-------------*
+// Time complexity = O(n^2)
+// Space complexity = O(1)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target));
