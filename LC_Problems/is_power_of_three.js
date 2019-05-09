0d6df3ecbb78d4678d29d876c37021223e17bd24
Niyati Desai <niyatihd@gmail.com>
2019-05-08 21:30:44 -0700

    Solve power of three

--- LC_Problems/is_power_of_three.js
+++ LC_Problems/is_power_of_three.js
@@ -14,3 +14,29 @@
 // Output: false
 // Follow up:
 // Could you do it without using any loop / recursion?
+
+function isPowerOfThree(n) {
+  if (n < 1) return false;
+
+  while (n >= 1) {
+    if (n === 1) {
+      return true;
+    } else if (n % 3 !== 0) {
+      return false;
+    }
+
+    n = n / 3;
+    console.log(n);
+  }
+}
+
+// *-------------*
+// Analysis:
+// *-------------*
+// Time complexity = O(n)
+// Space complexity = O(1)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
+console.log(isPowerOfThree(27));
