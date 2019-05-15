aaffd93a53d6911474e4457a05ee80763ae928c7
Niyati Desai <niyatihd@gmail.com>
2019-05-15 13:57:40 -0700

    solve move zeroes

---
+++ challenges/test1.js
@@ -0,0 +1,8 @@
+// number of islands, 1 = land and 0 = water
+
+arr = [[1, 1, 0, 1, 0], [1, 1, 1, 1, 0], [1, 0, 1, 0, 0], [1, 1, 0, 0, 0]];
+
+//iterating through outer loop
+//iterating through inner loop
+//pos = [x,y]
+[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1];
