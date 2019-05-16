beb9ae0c4e44562eba35be299582069c7ed3453c
Niyati Desai <niyatihd@gmail.com>
2019-05-16 11:30:46 -0700

    Solve in place shuffle

--- Problems/inplace_shuffle.js
+++ Problems/inplace_shuffle.js
@@ -3,14 +3,28 @@
 // Assume that you have a function getRandom(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
 
 function getRandom(floor, ceiling) {
-  return Math.random(floor, ceiling);
+  return Math.floor(Math.random() * (ceiling - floor) + floor);
 }
 
-console.log(getRandom(2, 8));
+// console.log(getRandom(2, 8));
 
 function inPlaceShuffle(arr) {
   //arr = [2,7,4,2,5,6,3]
   let pos = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let randomIdx = getRandom(pos, arr.length);
+
+    if (randomIdx !== pos) {
+      let temp = arr[pos];
+      arr[pos] = arr[randomIdx];
+      arr[randomIdx] = temp;
+    }
+
+    pos++;
+  }
+
+  return arr;
 }
 
 // *-------------*
@@ -22,3 +36,4 @@ function inPlaceShuffle(arr) {
 // *-------------*
 //   TEST CASES
 // *-------------*
+console.log(inPlaceShuffle([1, 2, 3, 4, 5, 6]));
