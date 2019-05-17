c20b5e7fb423a7e1f9973316fac9fdb6ee2a5752
Niyati Desai <niyatihd@gmail.com>
2019-05-17 12:24:07 -0700

    Solve repeated string

---
+++ HackerRankProblems/repeated_string.js
@@ -0,0 +1,38 @@
+// Lilah has a string, s, of lowercase English letters that she repeated infinitely many times.
+// Given an integer, n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+// For example, if the string s = 'abcac' and  n= 10, the substring we consider is 'abcacabcac', the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
+// Function Description
+// Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
+// repeatedString has the following parameter(s):
+// s: a string to repeat
+// n: the number of characters to consider
+// Input Format
+// The first line contains a single string,s .
+// The second line contains an integer, n.
+
+function countA(str) {
+  let count = 0;
+
+  str.split("").forEach(ele => {
+    if (ele === "a") count++;
+  });
+
+  return count;
+}
+
+function repeatedString(s, n) {
+  if (s.length === 1) return n;
+
+  const strLen = s.length;
+}
+
+// *-------------*
+// Analysis:
+// *-------------*
+// Time complexity = O(n)
+// Space complexity = O(1)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
+console.log(countA("abhuahagaba")); // => 5
