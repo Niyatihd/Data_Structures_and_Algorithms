2cc932afd0a4fa15209d07f9b4989e00ddfe0c61
Niyati Desai <niyatihd@gmail.com>
2019-05-20 15:02:11 -0700

    find rotation point

---
+++ InterviewCakeProblems/find_rotation_point.js
@@ -0,0 +1,28 @@
+// Write a function for finding the index of the "rotation point," which is where I started working from the beginning of the dictionary.This array is huge(there are lots of words I don't know) so we want to be efficient here.
+// const words = [
+//   'ptolemaic',
+//   'retrograde',
+//   'supplant',
+//   'undulate',
+//   'xenoepist',
+//   'asymptote',  // <-- rotates here!
+//   'babka',
+//   'banoffee',
+//   'engender',
+//   'karpatka',
+//   'othellolagkage',
+// ];
+
+// *-------------*
+// Solution:
+// *-------------*
+
+// *-------------*
+// Analysis:
+// *-------------*
+// Time complexity = O(log(n))
+// Space complexity = O(1)
+
+// *-------------*
+//   TEST CASES
+// *-------------*
