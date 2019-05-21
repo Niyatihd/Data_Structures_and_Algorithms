61935ae660323388b79b1d23463f44fb111abf99
Niyati Desai <niyatihd@gmail.com>
2019-05-20 18:58:23 -0700

    find rotation point

--- InterviewCakeProblems/find_rotation_point.js
+++ InterviewCakeProblems/find_rotation_point.js
@@ -35,10 +35,13 @@ function findRotationPoint(arr) {
       end = mid;
     }
 
-    if (start + 1 === end) {
+    if (start + 1 === end && end === arr.length - 1) {
+      return 0;
+    } else if (start + 1 === end) {
       break;
     }
   }
+
   return end;
 }
 
@@ -52,7 +55,6 @@ function findRotationPoint(arr) {
 //   TEST CASES
 // *-------------*
 console.log(findRotationPoint(["k", "v", "a", "b", "c", "d", "e", "g", "i"])); // => 2
-console.log(findRotationPoint(words)); // => 5
-console.log(findRotationPoint2(["k", "v", "a", "b", "c", "d", "e", "g", "i"])); // => 2
 //////////////////////////////  0    1    2    3    4    5    6    7    8
-console.log(findRotationPoint2(words)); // => 5
+console.log(findRotationPoint(words)); // => 5
+console.log(findRotationPoint(["a", "b", "c", "d"])); // => 0
